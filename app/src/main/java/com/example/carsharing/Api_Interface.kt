d00dea6f0dd45f6d0e5ccdc5539be5385e9832e6
WangWang <eeha8834@gmail.com>
2019-12-14 14:24:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/carsharing/Api_Interface.kt
@@ -0,0 +1,6 @@
+package com.example.carsharing
+
+interface Api_Interface {
+
+    
+}
---
+++ app/src/main/java/com/example/carsharing/lobby/LobbyActivity.kt
@@ -0,0 +1,13 @@
+package com.example.carsharing.lobby
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.carsharing.R
+
+class LobbyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lobby)
+    }
+}
